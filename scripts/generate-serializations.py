a72d976a2b4eb75029f67a2c392a53f298ec4711
Gabe Fierro <gtfierro@mines.edu>
2024-07-22 14:08:54 -0600

    more serialization

--- scripts/generate-serializations.py
+++ scripts/generate-serializations.py
@@ -12,3 +12,9 @@ for filename in glob.glob('static/schema/*/Brick.ttl'):
     # save a jsonld version too
     g.serialize(filename.replace('ttl', 'jsonld'), format='json-ld')
     print(f'Wrote {filename} to {filename.rstrip(".ttl")}')
+
+# do this for static/schema/Brick.ttl too
+g = rdflib.Graph()
+g.parse('static/schema/Brick.ttl', format='turtle')
+g.serialize('static/schema/Brick', format='xml')
+g.serialize('static/schema/Brick.jsonld', format='json-ld')
